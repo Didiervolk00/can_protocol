d1830129adeafd7275d3b245a02312282cde1ce0
Didier Volk <didiervolk@hotmail.com>
2022-10-26 13:01:44 +0200

    ASCII to HEX

--- read.c
+++ read.c
@@ -27,8 +27,9 @@ int main()
 {
     FILE* ptr;
     char ch;
+    char str[16];
+    int ch_count = 0;
  
-    // Opening file in reading mode
     ptr = fopen("test1.hex", "r");
  
     if (NULL == ptr) {
@@ -37,16 +38,29 @@ int main()
  
     printf("content of this file are \n");
  
-    // Printing what is written in file
-    // character by character using loop.
     do {
         ch = fgetc(ptr);
-        printf("%c", ch);  
+        strncat(str, &ch, 1);
+        ch_count++;
+            
+    } while (ch_count < sizeof(str));
 
+    // char ascii_str[] = "Hello world!";
+    //declare output string with double size of input string
+    //because each character of input string will be converted
+    //in 2 bytes
+    int len = strlen(str);
+    char hex_str[(len*2)+1];
+    
+    //converting ascii string to hex string
+    string2hexString(str, hex_str);
+    
+    printf("ascii_str: %s\n", str);
+    printf("hex_str: %s\n", hex_str);
 
-    } while (ch != EOF);
+    // printf("%s\n", str);
+    // printf("%ld\n", sizeof(str));
 
-    // Closing the file
     fclose(ptr);
     return 0;
 }
